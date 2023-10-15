b2443e98baa4600ee1caaa21806e72383410fa51
Fardin7864 <fardinhasan1888@gmail.com>
2023-10-15 23:58:36 +0600

    add login and signup

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,19 +1,35 @@
 import { Link } from "react-router-dom";
 import SocialLogin from "../../components/SocialLogin/SocialLogin";
+import { useContext } from "react";
+import { AuthContext } from "../../provider/GlobalAuthProv";
 
 const Login = () => {
+  const {logInwithEmail, success, faild} = useContext(AuthContext);
+
+  const handleLogin =(e) => { 
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(email, password)
+    logInwithEmail(email,password)
+    .then(() => { 
+      success("log in") })
+  .catch(() => {
+      faild()})
+   }
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col w-3/5">
           <h1 className="text-5xl font-bold font-rancho text-shadow-custom">Login now!</h1>
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <form className="card-body">
+          <form onSubmit={handleLogin} className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
               </label>
               <input
                 type="email"
+                name="email"
                 placeholder="email"
                 className="input input-bordered"
                 required
@@ -25,6 +41,7 @@ const Login = () => {
               </label>
               <input
                 type="password"
+                name="password"
                 placeholder="password"
                 className="input input-bordered"
                 required
